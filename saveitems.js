61581057102916d6ae5bf35ea87daeb08f56e364
JenniferZh <jenniferzhang0606@gmail.com>
2017-12-14 17:04:04 +0800

    增加分类编码浏览版本

--- saveitems.js
+++ saveitems.js
@@ -20,9 +20,9 @@ var saveItem = function(db, name, childs, parent, parent_list, attr, attr_all) {
     });
 };
 
-var saveClassify = function (db, json) {
+var saveJsonToCollection = function (db, collectionname, json) {
     return new Promise(function (resolve, reject) {
-        db.collection('Classcodes').insert(json, function (err, result) {
+        db.collection(collectionname).insert(json, function (err, result) {
             if(err) {
                 reject(err);
             } else {
@@ -41,17 +41,72 @@ function getFiles(folder) {
     return jsons;
 }
 
-testfolder = './classifycode';
-var jsons = getFiles(testfolder);
+function traserval(json, parent, parent_list, result) {
+    var cur = {}
+
+    cur.name = json.name;
+    cur.code = json.code;
+    cur.parent = parent;
+    cur.def = (json.def === undefined)? 'undefined':json.def;
+    cur.childs = [];
+
+    cur.parent_list = [];
+    for(var p = 0; p < parent_list.length; p++) {
+        cur.parent_list.push(parent_list[p]);
+    }
+    cur.parent_list.push(parent);
+
+
+    if(json.children.length !== 0) {
+        for(var i = 0; i < json.children.length; i++) {
+            cur.childs.push(json.children[i].name);
+            traserval(json.children[i], json.name, cur.parent_list, result);
+        }
+    }
+    result.push(cur);
+}
+
+function  generate_class_data() {
+    testfolder = './classifycode';
+    var jsons = getFiles(testfolder);
+    var myresult = [];
+    var root = {};
+
+    root.name = '铁路领域分类编码';
+    root.childs = [];
+    root.parent = 'null';
+    root.def = 'undefined';
+    root.code = 'undefined';
+    root.parent_list = [];
+
+    for(var i = 0; i < jsons.length; i++) {
+        root.childs.push(jsons[i].name);
+        traserval(jsons[i], '铁路领域分类编码', [] ,myresult);
+    }
+
+    myresult.push(root);
+
+    var Classcode = {};
+    Classcode.notsplit = jsons;
+    Classcode.split = myresult;
+
+    return Classcode;
+}
+
+
 
 MongoClient.connect(url, function (err, db) {
     if(err) {
         console.error(err);
     } else {
         var itemPromises = [];
+        var ClassCodeData = generate_class_data();
 
-        for(var i = 0; i < jsons.length; i++)
-            itemPromises.push(saveClassify(db, jsons[i]));
+        for(var i = 0; i < ClassCodeData.notsplit.length; i++)
+            itemPromises.push(saveJsonToCollection(db, 'Classcodes', ClassCodeData.notsplit[i]));
+
+        for(var i = 0; i < ClassCodeData.split.length; i++)
+            itemPromises.push(saveJsonToCollection(db, 'Classitems', ClassCodeData.split[i]));
 
         Promise.all(itemPromises).then(function () {
             db.close();
