229cd0ca91e92cfabd7a74b497473a4b34c77e83
JenniferZh <jenniferzhang0606@gmail.com>
2018-02-28 19:49:54 +0800

    公路独立

--- saveitems.js
+++ saveitems.js
@@ -36,6 +36,7 @@ function getFiles(folder) {
     var jsons = [];
     fs.readdirSync(folder).forEach(function (file)  {
         var ajson = JSON.parse(fs.readFileSync(folder+'/'+file, 'utf8'));
+        ajson.code = '';
         jsons.push(ajson)
     });
     return jsons;
@@ -44,8 +45,15 @@ function getFiles(folder) {
 function traserval(json, parent, parent_list, result) {
     var cur = {}
 
+
+
     cur.name = json.name;
     cur.code = json.code;
+
+    if(cur.code !== undefined)
+        cur.name = cur.name + cur.code.replace(/[\s.-]/g, '');
+
+
     cur.parent = parent;
     cur.def = (json.def === undefined)? 'undefined':json.def;
     cur.childs = [];
@@ -59,15 +67,15 @@ function traserval(json, parent, parent_list, result) {
 
     if(json.children.length !== 0) {
         for(var i = 0; i < json.children.length; i++) {
-            cur.childs.push(json.children[i].name);
-            traserval(json.children[i], json.name, cur.parent_list, result);
+            cur.childs.push(json.children[i].name+json.children[i].code.replace(/[\s.-]/g, ''));
+            traserval(json.children[i], json.name+json.code.replace(/[\s.-]/g, ''), cur.parent_list, result);
         }
     }
     result.push(cur);
 }
 
 function  generate_class_data() {
-    testfolder = './classifycode';
+    var testfolder = './classifycode';
     var jsons = getFiles(testfolder);
     var myresult = [];
     var root = {};
@@ -93,6 +101,33 @@ function  generate_class_data() {
     return Classcode;
 }
 
+function  generate_road_class_data() {
+    var testfolder = './classifycode-road';
+    var jsons = getFiles(testfolder);
+    var myresult = [];
+    var root = {};
+
+    root.name = '公路领域分类编码';
+    root.childs = [];
+    root.parent = 'null';
+    root.def = 'undefined';
+    root.code = 'undefined';
+    root.parent_list = [];
+
+    for(var i = 0; i < jsons.length; i++) {
+        root.childs.push(jsons[i].name);
+        traserval(jsons[i], '公路领域分类编码', [] ,myresult);
+    }
+
+    myresult.push(root);
+
+    var Classcode = {};
+    Classcode.notsplit = jsons;
+    Classcode.split = myresult;
+
+    return Classcode;
+}
+
 
 
 MongoClient.connect(url, function (err, db) {
@@ -101,12 +136,16 @@ MongoClient.connect(url, function (err, db) {
     } else {
         var itemPromises = [];
         var ClassCodeData = generate_class_data();
+        var ClassCodeRoadData = generate_road_class_data();
 
         for(var i = 0; i < ClassCodeData.notsplit.length; i++)
             itemPromises.push(saveJsonToCollection(db, 'Classcodes', ClassCodeData.notsplit[i]));
 
         for(var i = 0; i < ClassCodeData.split.length; i++)
             itemPromises.push(saveJsonToCollection(db, 'Classitems', ClassCodeData.split[i]));
+
+        for(var i = 0; i < ClassCodeRoadData.split.length; i++)
+            itemPromises.push(saveJsonToCollection(db, 'RoadClassitems', ClassCodeRoadData.split[i]));
 
         Promise.all(itemPromises).then(function () {
             db.close();
