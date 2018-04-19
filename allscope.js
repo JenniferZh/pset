e5ca0f0116fb46dcfb4c2c43e4e8d4f9ee5571a4
jennifer <jennifer@gmail.com>
2018-04-20 00:22:04 +0800

    allscope insert(not yet)

--- allscope.js
+++ allscope.js
@@ -1,6 +1,29 @@
 var MongoClient = require('mongodb').MongoClient,
     fs = require('fs'),
-    url = 'mongodb://localhost/mylibrary';
+    url = 'mongodb://localhost:27017/mylibrary';
 
 var json = JSON.parse(fs.readFileSync('./allscope/alldata.json', 'utf8'));
-console.log(json)
+var same = JSON.parse(fs.readFileSync('./allscope/relationsame.json', 'utf8'));
+var parent = JSON.parse(fs.readFileSync('./allscope/relationparent.json', 'utf8'));
+
+function addItemToCollection(db, collection, item) {
+    return new Promise(function(resolve, reject) {
+       db.collection(collection).insertOne(item, function(err, result) {
+
+       })
+    });
+}
+
+MongoClient.connect('url', function(err, db) {
+    db.collection('AllScopes').insertMany(json, function(err, result) {
+        console.log(result.insertedCount);
+    });
+    db.collection('RelationSame').insertMany(same, function(err, result) {
+        console.log(result.insertedCount);
+    });
+    db.collection('RelationParent').insertMany(parent, function(err, result) {
+        console.log(result.insertedCount);
+    });
+    db.close();
+
+});
