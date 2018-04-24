3ce13f488ee560d908fd55c6e9e4646e92328b6a
JenniferZh <jenniferzhang0606@gmail.com>
2018-04-24 12:32:03 +0800

    多领域数据生成allscopes relationparent relationsame表

--- allscope.js
+++ allscope.js
@@ -1,6 +1,10 @@
+/**
+ * 生成AllScopes, RalationParent, RelationSame三个collection的数据
+ */
+
 var MongoClient = require('mongodb').MongoClient,
     fs = require('fs'),
-    url = 'mongodb://localhost:27017/mylibrary';
+    url = 'mongodb://localhost/mylibrary';
 
 var json = JSON.parse(fs.readFileSync('./allscope/alldata.json', 'utf8'));
 var same = JSON.parse(fs.readFileSync('./allscope/relationsame.json', 'utf8'));
@@ -14,16 +18,25 @@ function addItemToCollection(db, collection, item) {
     });
 }
 
-MongoClient.connect('url', function(err, db) {
+MongoClient.connect(url, function(err, db) {
+
     db.collection('AllScopes').insertMany(json, function(err, result) {
-        console.log(result.insertedCount);
+        if(err) {
+            throw err;
+        }
+        db.collection('RelationSame').insertMany(same, function(err, result) {
+            if(err) {
+                throw err;
+            }
+            db.collection('RelationParent').insertMany(parent, function(err, result) {
+                if(err) {
+                    throw err;
+                }
+                db.close();
+            });
+        });
     });
-    db.collection('RelationSame').insertMany(same, function(err, result) {
-        console.log(result.insertedCount);
-    });
-    db.collection('RelationParent').insertMany(parent, function(err, result) {
-        console.log(result.insertedCount);
-    });
-    db.close();
+
+
 
 });
