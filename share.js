6fd37981accec3d21483708dd0e1d6c51f36cce8
JenniferZh <jenniferzhang0606@gmail.com>
2018-09-27 14:38:04 +0800

    feature:update share

--- share.js
+++ share.js
@@ -6,7 +6,7 @@ var json = JSON.parse(fs.readFileSync('./share.json', 'utf8'));
 var sharejson = [];
 var relationjson = [];
 var relationsame = [];
-var set = new Set();
+var set = new Map();
 var num = 1;
 
 function FormatNumberLength(num, length) {
@@ -26,9 +26,9 @@ json.forEach(function(item) {
     if(!set.has(item.a.name)) {
         var newshare = {};
         newshare.name = item.a.name;
-        set.add(item.a.name);
         newshare.code = FormatNumberLength(num++, 5);
         newshare.scope = 'GX';
+        set.set(item.a.name, newshare.code);
         sharejson.push(newshare);
         var relation = {};
         relation.parent = 'GX-00000';
@@ -43,34 +43,50 @@ json.forEach(function(item) {
         relationsame_b.a = 'GX-'+newshare.code;
         relationsame_b.b = item.b.code;
         relationsame.push(relationsame_b);
+    } else {
+        var sharecode = set.get(item.a.name);
+        var relationsame_a = {};
+        relationsame_a.a = 'GX-'+sharecode;
+        relationsame_a.b = item.a.code;
+        relationsame.push(relationsame_a);
+        var relationsame_b = {};
+        relationsame_b.a = 'GX-'+sharecode;
+        relationsame_b.b = item.b.code;
+        relationsame.push(relationsame_b);
     }
     //console.log(item);
-});
-
-MongoClient.connect(url, function(err, db) {
-
-    db.collection('RelationSame').insertMany(relationsame, function(err, result) {
-        if(err) {
-            throw err;
-        }
-        db.close();
-    });
 });
 
 
 // MongoClient.connect(url, function(err, db) {
 //
-//     db.collection('AllScopes').insertMany(sharejson, function(err, result) {
+//     db.collection('RelationSame').insertMany(relationsame, function(err, result) {
 //         if(err) {
 //             throw err;
 //         }
-//         db.collection('RelationParent').insertMany(relationjson, function(err, result) {
-//             if(err) {
-//                 throw err;
-//             }
-//             db.close();
-//         });
+//         db.close();
 //     });
 // });
+
+
+MongoClient.connect(url, function(err, db) {
+
+    db.collection('AllScopes').insertMany(sharejson, function(err, result) {
+        if(err) {
+            throw err;
+        }
+        db.collection('RelationParent').insertMany(relationjson, function(err, result) {
+            if(err) {
+                throw err;
+            }
+            db.collection('RelationSame').insertMany(relationsame, function(err, result) {
+                if(err) {
+                    throw err;
+                }
+                db.close();
+            });
+        });
+    });
+});
 
 
